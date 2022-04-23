9ece9285d380e68a436f8b27abd91d1e383a67ad
jgw2000 <jgw2007@vip.qq.com>
2022-04-23 17:31:57 +0800

    Site updated: 2022-04-23 17:31:53

--- js/pjax.js
+++ js/pjax.js
@@ -3,20 +3,19 @@
 const pjax = new Pjax({
   selectors: [
     'head title',
-    'head meta[name^="hexo-config-"]',
+    'script[type="application/json"]',
     '.main-inner',
     '.post-toc-wrap',
     '.languages',
     '.pjax'
   ],
-  analytics        : false,
-  cacheBust        : false,
-  scrollRestoration: false,
-  scrollTo         : !CONFIG.bookmark.enable
+  analytics: false,
+  cacheBust: false,
+  scrollTo : !CONFIG.bookmark.enable
 });
 
 document.addEventListener('pjax:success', () => {
-  pjax.executeScripts(document.querySelectorAll('script[data-pjax], .pjax script'));
+  pjax.executeScripts(document.querySelectorAll('script[data-pjax]'));
   NexT.boot.refresh();
   // Define Motion Sequence & Bootstrap Motion.
   if (CONFIG.motion.enable) {
@@ -26,8 +25,10 @@ document.addEventListener('pjax:success', () => {
       .add(NexT.motion.middleWares.postList)
       .bootstrap();
   }
-  const hasTOC = document.querySelector('.post-toc');
-  document.querySelector('.sidebar-inner').classList.toggle('sidebar-nav-active', hasTOC);
-  document.querySelector(hasTOC ? '.sidebar-nav-toc' : '.sidebar-nav-overview').click();
-  NexT.utils.updateSidebarPosition();
+  if (CONFIG.sidebar.display !== 'remove') {
+    const hasTOC = document.querySelector('.post-toc');
+    document.querySelector('.sidebar-inner').classList.toggle('sidebar-nav-active', hasTOC);
+    NexT.utils.activateSidebarPanel(hasTOC ? 0 : 1);
+    NexT.utils.updateSidebarPosition();
+  }
 });
